122e06aa7b9954cffc3677f3723f015a7051d340
woxQAQ <woxqaq@gmail.com>
2025-08-04 08:41:49 +0000

    fix nodejs entrypoint

--- src/runtime/nodejs/entrypoint.js
+++ src/runtime/nodejs/entrypoint.js
@@ -1,6 +1,5 @@
-const koffi = require("koffi")
-const lib = koffi.load("./libseccomp_injector_python.so")
-const libseccomp = lib.func("int inject_seccomp_profile(int, int)")
+const libseccomp = require("./libseccomp_injector_nodejs.so")
+const inject_seccomp_profile = libseccomp.inject_seccomp_profile
 
 const decryptCode = (code, key) => {
   const encrypted = Buffer.from(code, 'base64');
@@ -17,5 +16,5 @@ const decryptCode = (code, key) => {
 const argv = process.argv
 
 const code = decryptCode('{{ code }}', argv[3])
-libseccomp(argv[4], argv[5])
+inject_seccomp_profile(parseInt(argv[4]), parseInt(argv[5]))
 eval(code)
